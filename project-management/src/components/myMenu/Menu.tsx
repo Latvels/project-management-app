5c9712957b78e3966213be4ab7605dd39f14f057
Tatsiana <t_a_b@tut.by>
2022-05-10 22:33:55 +0300

    feat: add menu component

--- project-management/src/components/myMenu/Menu.tsx
+++ project-management/src/components/myMenu/Menu.tsx
@@ -1,16 +1,41 @@
 import React, { useState } from 'react';
-import { Drawer, Box, List, ListItemButton, ListItemIcon, ListItem, ListItemText, IconButton } from '@mui/material';
+// import { Drawer, Box, List, ListItemButton, ListItemIcon, ListItem, ListItemText, IconButton } from '@mui/material';
+
 import HomeIcon from '@mui/icons-material/Home';
 import MenuIcon from '@mui/icons-material/Menu';
 import EditIcon from '@mui/icons-material/Edit';
 import DashboardCustomizeIcon from '@mui/icons-material/DashboardCustomize';
-import LanguageIcon from '@mui/icons-material/Language';
+// import LanguageIcon from '@mui/icons-material/Language';
+import { NavLink } from 'react-router-dom';
+import { Box, SpeedDial, SpeedDialAction } from '@mui/material';
+
+const actions = [
+    { icon: <HomeIcon />, name: 'Go To Main Page' },
+    { icon: <EditIcon />, name: 'Edit Profile' },
+    { icon: <DashboardCustomizeIcon />, name: 'Create New Board' },
+  ];
 
 function MyMenu() {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  // const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   return (
-    <>
-      <IconButton
+    <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
+    <SpeedDial
+      ariaLabel="SpeedDial basic example"
+      sx={{ position: 'absolute', bottom: 16, right: 16 }}
+      icon={<MenuIcon />}
+    >
+      {actions.map((action) => (
+        <NavLink to="/">
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+          />
+        </NavLink>
+      ))}
+    </SpeedDial>
+  </Box>
+      {/* <IconButton
           size="large"
           edge="start"
           color="inherit"
@@ -24,14 +49,16 @@ function MyMenu() {
       <Drawer anchor='left' open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
         <Box p={2} width='250px' textAlign='left' role='presentation'>
           <List>
-            <ListItem>
-              <ListItemButton>
-                <ListItemIcon>
-                  <HomeIcon />
-                </ListItemIcon>
-              </ListItemButton>
-              <ListItemText>Go To Main Page</ListItemText>
-            </ListItem>
+            <NavLink to="/">
+              <ListItem>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <HomeIcon />
+                  </ListItemIcon>
+                </ListItemButton>
+                <ListItemText>Go To Main Page</ListItemText>
+              </ListItem>
+            </NavLink>
             <ListItem>
               <ListItemButton>
                 <ListItemIcon>
@@ -50,9 +77,8 @@ function MyMenu() {
             </ListItem>
           </List>
         </Box>
-      </Drawer>
-    </>
-  )
+      </Drawer> */}
+  );
 }
 
 export default MyMenu;
