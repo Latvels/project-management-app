1c5888a0f5700088fa34b927973b302390544525
Tatsiana <t_a_b@tut.by>
2022-05-12 12:58:54 +0300

    Merge branch 'develop' into header

--- project-management/src/components/App/App.tsx
+++ project-management/src/components/App/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import './App.css';
 import MainRoutes from '../../routes/routes';
-import { useTranslation, Trans } from 'react-i18next';
-import {availableLanguages} from '../../services/i18n';
+// import { useTranslation, Trans } from 'react-i18next';
+// import {availableLanguages} from '../../services/i18n';
+import Header from '../header/Header';
 
 export default function App() {
-  const { t, i18n  } = useTranslation();
+  // const { t, i18n  } = useTranslation();
 
   return (
     <div className="App">
-      <select defaultValue={i18n.language} onChange={(e) => i18n.changeLanguage(e.target.value)}>
+      {/* <select defaultValue={i18n.language} onChange={(e) => i18n.changeLanguage(e.target.value)}>
           {availableLanguages.map((language) => (
             <option key={language}>{language}</option>
-          ))}
+            ))}
         </select>
       <p>
         <Trans i18nKey="description:part1"></Trans>
       </p>
-      <p>{t('description:part2')}</p>
+      <p>{t('description:part2')}</p> */}
+      <Header></Header>
       <MainRoutes />
     </div>
   );
