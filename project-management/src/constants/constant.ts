7d22e8fb3bee3309eadffd89af051e1e0c70bab9
Evgeniy <Latvelsit@gmail.com>
2022-05-13 15:58:56 +0300

    api

---
+++ project-management/src/constants/constant.ts
@@ -0,0 +1,8 @@
+export const API_ROUTES = {
+	getUser: '/signup',
+};
+
+export const CONFIG = {
+	basicURL: 'https://react-project-management-task.herokuapp.com',
+	token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJmOTYxMmJiOC00Mjc1LTRiMzUtYjAyYy02ZmZhYjQ0ZWQ2N2UiLCJsb2dpbiI6ImFkbWluIiwiaWF0IjoxNjUyMzYzMTQ2fQ.ZBXVRVqBVVFubTW4AOhB7tzsmZWW1d3K5i8czg4c94A',
+};
