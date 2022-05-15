7270a79500d669736af0427254587a8b086f6302
Tatsiana <t_a_b@tut.by>
2022-05-15 22:17:12 +0300

    feat: disable submit button if form invalid

--- project-management/src/components/CreateNewBoardForm/CreateNewBoardFormFormik.tsx
+++ project-management/src/components/CreateNewBoardForm/CreateNewBoardFormFormik.tsx
@@ -3,15 +3,18 @@ import { Formik, Form, Field } from 'formik';
 import { TextField } from 'formik-mui';
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
 import { createBoards } from '../../api/api';
+import { setIsCreateNewBoardModalOpen } from '../../store/action/appStateAction';
 import './createNewBoardFormFormik.scss';
 
-interface Values {
+interface IValues {
   title: string;
   description: string;
 }
 
 function CreateNewBoardFormFormik() {
+  const appDispatch = useDispatch();
   const {t} = useTranslation();
   const titleLabel = t('createNewBoardForm:boardTitle');
   const descriptionLabel = t('createNewBoardForm:boardDescription');
@@ -19,30 +22,39 @@ function CreateNewBoardFormFormik() {
   const required = t('createNewBoardForm:validation.required');
   const minValue = t('createNewBoardForm:validation.minValue');
 
+  const [isButtonDisabled, setIsButtonDisabled] = React.useState(true);
+
+  const validateForm = (values: IValues): Partial<IValues> => {
+    const errors: Partial<IValues> = {};
+    if (!values.title) {
+      errors.title = required;
+    } else if (values.title.length < 3) {
+      errors.title = minValue;
+    } else if (!values.description) {
+      errors.description = required;
+    }
+    if (!errors.title && !errors.description) {
+      setIsButtonDisabled(false);
+    }
+    return errors;
+  }
+
+  const initialValues = {
+    title: '',
+    description: '',
+  }
 
   return (
     <Formik
-      initialValues={{
-        title: '',
-        description: '',
-      }}
-      validate={(values) => {
-        const errors: Partial<Values> = {};
-        if (!values.title) {
-          errors.title = required;
-        } else if (values.title.length < 3) {
-          errors.title = minValue;
-        } else if (!values.description) {
-          errors.description = required;
-        }
-        return errors;
-      }}
-      onSubmit={async (values, { setSubmitting }) => {
-          setSubmitting(false);
-          console.log(values);
-          const req = await createBoards(values);
-          console.log('req', req);
-      }}
+      initialValues={initialValues}
+      validate={validateForm}
+      onSubmit={async (values: IValues, {setSubmitting}) => {
+        setSubmitting(false);
+        console.log(values);
+        const req = await createBoards(values);
+        console.log('req', req);
+        //todo обработать ошибку создания борды
+        appDispatch(setIsCreateNewBoardModalOpen(false));}}
     >
       {({ submitForm, isSubmitting }) => (
         <Form className="form">
@@ -72,7 +84,7 @@ function CreateNewBoardFormFormik() {
           <Button
             variant="outlined"
             color="info"
-            disabled={isSubmitting}
+            disabled={isButtonDisabled}
             onClick={submitForm}
             type="submit"
           >
