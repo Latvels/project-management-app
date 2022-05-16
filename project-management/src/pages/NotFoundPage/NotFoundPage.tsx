2af5376bf34fb2713e705ae686bd7a88e2add524
Evgeniy <Latvelsit@gmail.com>
2022-05-16 10:17:40 +0300

    fix: 404 page

--- project-management/src/pages/NotFoundPage/NotFoundPage.tsx
+++ project-management/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Box, Button } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 function NotFoundPage() {
+  const {t} = useTranslation();
+
   return (
-    <div>
+    <Box sx={{
+      display: 'flex',
+      flexFlow: 'column',
+      alignItems: 'center',
+      margin: '5rem',
+      fontSize: '6rem',
+    }}>
       <div>404</div>
       <div>This pages is not defined</div>
-    </div>
+      <br/>
+      <Button variant="text" size="large" sx={{fontSize: '6rem'}}>
+        <NavLink to="/mainPage" className="menu__navLink">
+          {t('header:menuItem1')}
+        </NavLink>
+      </Button>
+    </Box>
   );
 }
 
