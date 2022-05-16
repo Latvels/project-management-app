2d07669a0a848a7dc04ee361b048125dcab3abea
Evgeniy <Latvelsit@gmail.com>
2022-05-16 10:25:32 +0300

    fix: notFoundPAge

--- project-management/src/pages/NotFoundPage/NotFoundPage.tsx
+++ project-management/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -12,12 +12,12 @@ function NotFoundPage() {
       flexFlow: 'column',
       alignItems: 'center',
       margin: '5rem',
-      fontSize: '6rem',
+      fontSize: '4rem',
     }}>
       <div>404</div>
-      <div>This pages is not defined</div>
+      <div>{t('errors:pagesNotDefined')}</div>
       <br/>
-      <Button variant="text" size="large" sx={{fontSize: '6rem'}}>
+      <Button variant="text" size="large" sx={{fontSize: '4rem'}}>
         <NavLink to="/mainPage" className="menu__navLink">
           {t('header:menuItem1')}
         </NavLink>
