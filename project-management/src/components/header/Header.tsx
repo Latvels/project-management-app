b708229247c5a69ea813ee523298a27eb659883c
Tatsiana <t_a_b@tut.by>
2022-05-21 11:46:55 +0300

    fix: adjust sticky header

--- project-management/src/components/header/Header.tsx
+++ project-management/src/components/header/Header.tsx
@@ -3,14 +3,21 @@ import { AppBar, Box, Toolbar, Typography, Button } from '@mui/material';
 import { SelectLanguage, MyMenu } from '../compunents';
 import './header.scss';
 import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../store/store';
+import { RootState } from '../../store/reducer/reducer';
+import { useSelector } from 'react-redux';
+import { Root } from 'react-dom/client';
 
 function Header() {
+  const appDispatch = useDispatch<AppDispatch>();
+  const appState = useSelector((state: RootState) => state.appState);
   const {t} = useTranslation();
   const headerRef: React.RefObject<HTMLElement> | null = useRef(null);
 
   useEffect(() => {
     checkScroll() ? addSticky() : delSticky();
-  });
+  },[appDispatch, appState]);
 
   const checkScroll = (): boolean => {
     return document.body.offsetHeight > window.innerHeight;
@@ -27,7 +34,7 @@ function Header() {
   };
   
   return (
-    <Box ref={headerRef} sx={{ flexGrow: 1, width: '100%', top: 0 }} className="header" component="div">
+    <Box ref={headerRef} sx={{ flexGrow: 1, width: '100%', top: 0, zIndex: 100 }} className="header" component="div">
       <AppBar position="static" className="header__appBar">
         <Toolbar>
           <MyMenu />
