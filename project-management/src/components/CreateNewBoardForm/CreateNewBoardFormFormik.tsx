2ed32c275bf670b5190e9f63dcf61dafc9a5ffa9
Tatsiana <t_a_b@tut.by>
2022-05-21 11:45:13 +0300

    fix: correct validation for createNewBoardForm

--- project-management/src/components/CreateNewBoardForm/CreateNewBoardFormFormik.tsx
+++ project-management/src/components/CreateNewBoardForm/CreateNewBoardFormFormik.tsx
@@ -13,6 +13,7 @@ import { AppDispatch } from '../../store/store';
 import { RootState } from '../../store/reducer/reducer';
 import { Error } from '../../typings/typings';
 import { err } from '../../utils/showBasicAlerts';
+
 interface IValues {
   title: string;
   description: string;
@@ -27,34 +28,41 @@ function CreateNewBoardFormFormik() {
   const required = t('formValidation:required');
   const minValue = t('formValidation:minValue');
   const maxValue = t('formValidation:maxValue');
+  const maxValueDescription = t('formValidation:maxValueDescription');
 
   const [isButtonDisabled, setIsButtonDisabled] = React.useState(true);
-  // Вот так получается обрабатывать ошибки с сервера
   const errorMessage = useSelector((state: RootState) => state.board.error) as Error;
-  // const err = (errorMessage:Error)=> {
-  //   const { message } = errorMessage
-  //   if (message !== '') {
-  //     return <BasicAlerts error={errorMessage}/>
-  //   }
-  // }
 
   const validateForm = (values: IValues): Partial<IValues> => {
     const errors: Partial<IValues> = {};
-    setIsButtonDisabled(true);
-    if (!values.title) {
-      errors.title = required;
-    } else if (values.title.length < 3) {
-      errors.title = minValue;
-    } else if (values.title.length > 12) {
-      errors.title = maxValue;
-    } else if (!values.description) {
-      errors.description = required;
+
+    function checkFormField(key: keyof IValues) {
+      if (!values[key]) {
+        errors[key] = required;
+      } else if (values[key].length < 3) {
+        errors[key] = minValue;
+      }
+    };
+
+    function checkMaxLength(key: keyof IValues) {
+      if (key === 'title' && values[key].length > 15) {
+        errors.title = maxValue;
+      }
+      if (key === 'description' && values[key].length > 30) {
+        errors.description = maxValueDescription;
+      }
     }
+
+    setIsButtonDisabled(true);
+    checkFormField('title');
+    checkFormField('description');
+    checkMaxLength('title');
+    checkMaxLength('description');
     if (!errors.title && !errors.description) {
       setIsButtonDisabled(false);
     }
     return errors;
-  }
+}
 
   const initialValues = {
     title: '',
@@ -68,9 +76,9 @@ function CreateNewBoardFormFormik() {
         validate={validateForm}
         onSubmit={async (values: IValues, {setSubmitting}) => {
           setSubmitting(false);
-        // appDispatch(setIsCreateNewBoardModalOpen(false));
         appDispatch(setIsPreloaderOpen(true));
         await appDispatch(createBoard(values));
+        window.location.reload();
         appDispatch(setIsPreloaderOpen(false));
         if(errorMessage.message === '') {
           appDispatch(setIsCreateNewBoardModalOpen(false));
@@ -102,7 +110,6 @@ function CreateNewBoardFormFormik() {
             color="info"
             disabled={isButtonDisabled}
             onClick={submitForm}
-            type="submit"
           >
             {buttonText}
           </Button>
