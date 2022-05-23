1252cd36f9b4a76c594332455cc1de9ca53a2c52
Chizh_OASU <elizavetka.chizh@gmail.com>
2022-05-23 16:30:37 +0300

    feat: add changes for routers

--- project-management/src/routes/routes.tsx
+++ project-management/src/routes/routes.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 // import { Dashboard } from '../components/compunents'
-import {NotFoundPage, WelcomePage, MainPage, BoardPage, LoginPage, Registration} from '../pages/pages';
+import {
+  NotFoundPage,
+  WelcomePage,
+  MainPage,
+  BoardPage,
+  LoginPage,
+  Registration,
+} from '../pages/pages';
 
 const MainRoutes = () => (
   <Routes>
     <Route path="*" element={<Navigate replace to="404" />} />
     <Route path="/" element={<WelcomePage />} />
-    <Route path="/mainPage" element={<MainPage />} />
-    <Route path="/boardPage" element={<BoardPage />} />
+    <Route path="/mainpage" element={<MainPage />} />
+    <Route path="/boardpage" element={<BoardPage />} />
     <Route path="/404" element={<NotFoundPage />} />
     <Route path="/signup" element={<Registration />} />
     <Route path="/signin" element={<LoginPage />} />
