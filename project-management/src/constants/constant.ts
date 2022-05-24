2039bd124ea699fd8f67e4974e11296ed02c9f92
Evgeniy <Latvelsit@gmail.com>
2022-05-24 09:46:41 +0300

    fix

--- project-management/src/constants/constant.ts
+++ project-management/src/constants/constant.ts
@@ -3,7 +3,7 @@ export const API_ROUTES = {
 };
 
 export const CONFIG = {
-  basicURL: 'https://rs-project-management.herokuapp.com/', //  'https://react-project-management-task.herokuapp.com',
+  basicURL: 'https://rs-project-management.herokuapp.com', //  'https://react-project-management-task.herokuapp.com',
   token:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJmOTYxMmJiOC00Mjc1LTRiMzUtYjAyYy02ZmZhYjQ0ZWQ2N2UiLCJsb2dpbiI6ImFkbWluIiwiaWF0IjoxNjUyMzYzMTQ2fQ.ZBXVRVqBVVFubTW4AOhB7tzsmZWW1d3K5i8czg4c94A',
 };
